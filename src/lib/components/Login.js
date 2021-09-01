24f51170710b7b6caee78902ddd81301400b27f1
osiris <wendyosi25@gmail.com>
2021-08-31 19:45:34 -0500

    cambioCompu

--- src/lib/components/Login.js
+++ src/lib/components/Login.js
@@ -1,11 +1,20 @@
+// eslint-disable-next-line import/no-cycle
+import { onNavigate } from '../../main.js';
+
 export const Login = () => {
   const Homediv = document.createElement('div');
-  Homediv.textContent = 'Bienvenido al Login';
+  Homediv.textContent = 'Inicia sesión';
+  const username = document.createElement('input');
+  username.placeholder = 'Email';
+  const password = document.createElement('input');
+  password.placeholder = 'Password';
   const buttonHome = document.createElement('button');
 
-  buttonHome.textContent = 'Regresar al Home';
+  buttonHome.textContent = 'Regresar';
+  buttonHome.addEventListener('click', () => onNavigate('/'));
 
   Homediv.appendChild(buttonHome);
-
+  Homediv.appendChild(username);
+  Homediv.appendChild(password);
   return Homediv;
 };
