5cf3f300b443057387599a0c1c9ed1871bd6b404
Daniela Torres <torreslz.dan@gmail.com>
2021-09-24 15:23:51 -0500

    setPersistence incomplete

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -5,55 +5,63 @@ export const Wall = () => {
   const wallDiv = document.createElement('div');
   wallDiv.id = 'wallDiv';
 
-  const btnLogOut = document.createElement('button');
-  btnLogOut.textContent = 'Log out';
+  const user = getUser();
+  if (user) {
+    const displayName = user.displayName;
+    console.log(displayName);
 
-  const postBox = document.createElement('div');
-  postBox.id = 'postBox';
+    const logoWall = document.createElement('img');
+    logoWall.setAttribute('src', 'https://firebasestorage.googleapis.com/v0/b/pata-de-perro-3a9dd.appspot.com/o/logo-pata-de-perro-red.png?alt=media&token=dc5ad00d-8d94-4a1b-88a2-1bf72802fb04');
+    logoWall.setAttribute('alt', 'logo pata de perro');
+    logoWall.classList.add('logoWall');
 
-  const labelUser = document.createElement('label');
-  labelUser.textContent = 'Usuario';
+    const labelUser = document.createElement('label');
+    labelUser.innerText = displayName;
 
-  const labelDate = document.createElement('label');
-  labelDate.textContent = 'Fecha';
+    const btnLogOut = document.createElement('button');
+    btnLogOut.textContent = 'Log out';
 
-  const postUser = document.createElement('textarea');
-  postUser.id = 'post';
-  postUser.placeholder = 'Escribe tus consejos aqui';
+    const postBox = document.createElement('div');
+    postBox.id = 'postBox';
 
-  const errorText = document.createElement('label');
-  errorText.classList.add('errorTextLabel');
+    const postUser = document.createElement('textarea');
+    postUser.id = 'post';
+    postUser.placeholder = 'Escribe tus consejos aqui';
 
-  const btnPublish = document.createElement('button');
-  btnPublish.id = 'btnPublish';
-  btnPublish.textContent = 'Publicar';
+    const errorText = document.createElement('label');
+    errorText.classList.add('errorTextLabel');
 
-  window.onload = loadPosts();
+    const btnPublish = document.createElement('button');
+    btnPublish.id = 'btnPublish';
+    btnPublish.textContent = 'Publicar';
 
-  btnLogOut.addEventListener('click', () => {
-    logOut();
-  });
+    window.onload = loadPosts();
 
-  btnPublish.addEventListener('click', (e) => {
-    e.preventDefault();
-    const textUser = wallDiv.querySelector('#post').value;
-    const user = getUser().displayName;
-    const newPost = (textUser === '')
-      ? errorText.textContent = 'No has escrito nada aún'
-      : posts(user, textUser)
-        .then((result) => {
-          wallDiv.querySelector('#post').value = '';
-          errorText.textContent = '';
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    return newPost;
-  });
+    btnLogOut.addEventListener('click', () => {
+      logOut();
+    });
 
-  wallDiv.appendChild(btnLogOut);
-  wallDiv.appendChild(postBox);
-  postBox.append(labelUser, labelDate, postUser, errorText, btnPublish, divPosts);
+    btnPublish.addEventListener('click', (e) => {
+      e.preventDefault();
+      const textUser = wallDiv.querySelector('#post').value;
+      console.log(user);
+      const newPost = (textUser === '')
+        ? errorText.textContent = 'No has escrito nada aún'
+        : posts(user.displayName, textUser)
+          .then((result) => {
+            wallDiv.querySelector('#post').value = '';
+            errorText.textContent = '';
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      return newPost;
+    });
 
+    wallDiv.append(logoWall, labelUser, btnLogOut, postBox);
+    postBox.append(postUser, errorText, btnPublish, divPosts);
+  } else {
+    console.log('error');
+  }
   return wallDiv;
 };
--- src/utils/createPost.js
+++ src/utils/createPost.js
@@ -24,14 +24,40 @@ export const loadPosts = async () => {
       btnDelete.classList.add('btnDelete');
       btnDelete.dataset.id = contentPost.id;
 
+      const btnsDelete = divPosts.querySelectorAll('.btnDelete');
+      btnsDelete.forEach((btn) => {
+        btn.addEventListener('click', () => {
+          const modalContainer = document.createElement('div');
+          modalContainer.classList.add('modalContainer');
+
+          const modal = document.createElement('div');
+          modal.classList.add('modal');
+
+          const msgDelete = document.createElement('p');
+          msgDelete.classList.add('msgDelete');
+          msgDelete.textContent = '¿Estás seguro de querer borrar este post?';
+
+          const btnMsgDelete = document.createElement('button');
+          btnMsgDelete.classList.add('btnMsgDelete');
+          btnMsgDelete.textContent = 'Eliminar';
+
+          btnMsgDelete.addEventListener('click', async (e) => {
+            try {
+              await deletePost(contentPost.id);
+              modalContainer.classList.remove('modalContainer');
+            } catch (error) {
+              alert('Ha ocurrido un error. Intenta más tarde');
+            }
+          });
+
+          divPosts.appendChild(modalContainer);
+          modalContainer.appendChild(modal);
+          modal.append(msgDelete, btnMsgDelete);
+        });
+      });
+
       divPosts.appendChild(divPost);
       divPost.append(postUsername, areaPost, btnDelete);
-    });
-    const btnsDelete = divPosts.querySelectorAll('.btnDelete');
-    btnsDelete.forEach((btn) => {
-      btn.addEventListener('click', async (e) => {
-        await deletePost(e.target.dataset.id);
-      });
     });
   });
 };
