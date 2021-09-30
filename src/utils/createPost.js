30b3f32cd8ef933225bad01d414f29e04ac00256
Daniela Torres <torreslz.dan@gmail.com>
2021-09-30 14:16:35 -0500

    delegation delete

--- src/utils/createPost.js
+++ src/utils/createPost.js
@@ -10,6 +10,7 @@ export const loadPosts = async () => {
       contentPost.id = doc.id;
 
       const divPost = document.createElement('div');
+      divPost.classList.add('divPost');
 
       const postUsername = document.createElement('h3');
       postUsername.classList.add('postUn');
@@ -24,47 +25,47 @@ export const loadPosts = async () => {
       btnDelete.textContent = 'eliminar';
       btnDelete.dataset.id = contentPost.id;
 
+      const modalContainer = document.createElement('div');
+      modalContainer.classList.add('modalContainer');
+      modalContainer.style.display = 'none';
+      const modal = document.createElement('div');
+      modal.classList.add('modal');
+      const msgDelete = document.createElement('p');
+      msgDelete.classList.add('msgDelete');
+      msgDelete.textContent = '¿Estás seguro de querer borrar este post?';
+      const btnMsgDelete = document.createElement('button');
+      btnMsgDelete.classList.add('btnMsgDelete');
+      btnMsgDelete.textContent = 'Eliminar';
+      btnMsgDelete.dataset.id = contentPost.id;
+      const btnMsgCancel = document.createElement('button');
+      btnMsgCancel.textContent = 'Cancelar';
+
+      divPosts.append(divPost);
+      divPost.append(postUsername, areaPost, btnDelete, modalContainer);
+      modalContainer.appendChild(modal);
+      modal.append(msgDelete, btnMsgDelete, btnMsgCancel);
+      /*     const btnsDelete = divPosts.querySelectorAll('.btnDelete');
+    btnsDelete.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        console.log(e.target.dataset.id);
+      });
+    }); */
       const btnsDelete = divPosts.querySelectorAll('.btnDelete');
+      const prueba = divPosts.querySelector('.modalContainer');
       btnsDelete.forEach((btn) => {
-        btn.addEventListener('click', () => {
-          const modalContainer = document.createElement('div');
-          modalContainer.classList.add('modalContainer');
-
-          const modal = document.createElement('div');
-          modal.classList.add('modal');
-
-          const msgDelete = document.createElement('p');
-          msgDelete.classList.add('msgDelete');
-          msgDelete.textContent = '¿Estás seguro de querer borrar este post?';
-
-          const btnMsgDelete = document.createElement('button');
-          btnMsgDelete.classList.add('btnMsgDelete');
-          btnMsgDelete.textContent = 'Eliminar';
-
-          const btnMsgCancel = document.createElement('button');
-          btnMsgCancel.textContent = 'Cancelar';
-
-          btnMsgDelete.addEventListener('click', async (e) => {
-            try {
-              await deletePost(contentPost.id);
-              modalContainer.classList.remove('modalContainer');
-            } catch (error) {
-              alert('Ha ocurrido un error. Intenta más tarde');
-            }
+        btn.addEventListener('click', (e) => {
+          prueba.style.display = 'block';
+          const pruebados = () => deletePost(e.target.dataset.id);
+          prueba.addEventListener('click', (event) => {
+            console.log(event.target);
+            if (event.target.classList.contains('btnMsgDelete')) {
+              console.log('soy el boton eliminar');
+              pruebados();
+              prueba.style.display = 'none';
+            } else { console.log('aaaaaaa'); }
           });
-
-          btnMsgCancel.addEventListener('click', () => {
-            modalContainer.remove('modalContainer');
-          });
-
-          divPosts.appendChild(modalContainer);
-          modalContainer.appendChild(modal);
-          modal.append(msgDelete, btnMsgDelete, btnMsgCancel);
         });
       });
-
-      divPosts.appendChild(divPost);
-      divPost.append(postUsername, areaPost, btnDelete);
     });
-  });
+	    });
 };
